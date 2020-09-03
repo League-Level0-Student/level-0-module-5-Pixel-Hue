0326be9b0bd78c1b3e3cf6eabec93ece1832b4f5
root <root@dc0384b67070>
2020-09-03 23:56:16 +0000

    sadas

---
+++ src/_01_algorithms/_1_prime_or_not/PrimeOrNot.java
@@ -0,0 +1,14 @@
+package _01_algorithms._1_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class PrimeOrNot {
+public static void main(String[] args) {
+	for (int i = 0; i < 999; i++) {
+	String A = JOptionPane.showInputDialog("Type in a whole number");
+	int b = Integer.parseInt(A);
+	
+	
+}
+}
+}
