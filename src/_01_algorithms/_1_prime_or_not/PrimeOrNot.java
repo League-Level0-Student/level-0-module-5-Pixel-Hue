7132f7a028b3fa37ff90d35d7333e773034d86d1
root <root@06aee389e54e>
2020-09-10 23:56:26 +0000

    asdf

--- src/_01_algorithms/_1_prime_or_not/PrimeOrNot.java
+++ src/_01_algorithms/_1_prime_or_not/PrimeOrNot.java
@@ -4,11 +4,20 @@ import javax.swing.JOptionPane;
 
 public class PrimeOrNot {
 public static void main(String[] args) {
-	for (int i = 0; i < 999; i++) {
 	String A = JOptionPane.showInputDialog("Type in a whole number");
 	int b = Integer.parseInt(A);
+	boolean a = false;
+	for (int i = 2; i < b; i++) {
+	if (b % i == 0) {
+		System.out.println("not prime");
+		a = true;
+	}
 	
 	
 }
+	if (a == false) {
+		System.out.println("prime");
+	}
+	
 }
 }
--- src/_01_algorithms/_3_goofy_names/GoofyNames.java
+++ src/_01_algorithms/_3_goofy_names/GoofyNames.java
@@ -9,26 +9,39 @@ import javax.swing.JOptionPane;
 public class GoofyNames {
 	public static void main(String[] args) {
 		String goofyName = "";
-
+int c = 1;
 		// 1. Ask the user to enter their name
-
+String name = JOptionPane.showInputDialog("Enter your name below");
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
-
+System.out.println(name.toUpperCase());
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
+for (int i = 0; i < name.length(); i++) {
+	System.out.println(name.charAt(i));
+
 
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
+            String a = name.substring(i, i + 1);
+            c = i;
+            if (c % 2 == 0) {
+            	a.toUpperCase();
+            	
+            }
+            else {
+            	a.toLowerCase();
+            	
+            }
+          System.out.println(a);
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
-
+        
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
 
-		
+}
 		// 7. Use pop-up to show user their Goofy name
 
 	}
