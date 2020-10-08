5ce108660412144bd5785425cf604181db11fca6
root <root@f5fe811094bc>
2020-10-08 23:56:17 +0000

    loop gauntlet

--- src/_02_nested_loops/_3_for_loop_gauntlet/LoopGauntlet.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/LoopGauntlet.java
@@ -2,33 +2,65 @@ package _02_nested_loops._3_for_loop_gauntlet;
 
 public class LoopGauntlet {
 	public static void main(String[] args) {
-		for (int i = 0; i < 101; i++) {
-			System.out.println(i);
-		}
-		for (int i = 100; i > -1; i--) {
-			System.out.println(i);
-		}
-		for (int i = 1; i < 101; i++) {
-			if (i % 2 == 0) {
-				System.out.println(i + " is even!");
-			}else {
-				System.out.println(i + " is odd!");
+//		for (int i = 0; i < 101; i++) {
+//			System.out.println(i);
+//		}
+//		for (int b = 100; b > -1; b--) {
+//			System.out.println(b);
+//		}
+//		for (int h = 1; h < 101; h++) {
+//			if (h % 2 == 0) {
+//				System.out.println(h + " is even!");
+//			} else {
+//				System.out.println(h + " is odd!");
+//			}
+//		}
+//		for (int i1 = 2; i1 < 501; i1++) {
+//			if (i1 % 2 == 0) {
+//				System.out.println(i1 + " is even!");
+//			} else {
+//				System.out.println(i1 + " is odd!");
+//			}
+//		
+//
+//		}
+//		int a = 7;
+//		for (int g = 0; g < 111; g++) {
+//			System.out.println(a);
+//			a += 7;
+//			
+//		}
+//		int year = 2008;
+//		for (int j = 0; j < 13; j++) {
+//			System.out.println("In " + year + " I was " + j);
+//			year++;
+//		}
+//		int o = -1;
+//		int p = 0;
+//		for (int i = 0; i < 9; i++) {
+//			
+//			
+//				if (i % 3 == 0) {
+//					o++;	
+//				}
+//			
+//			
+//				p++;
+//				if (p == 4) {
+//					p = 1;
+//				}
+//			
+//			System.out.println(o + " " + p);
+//		}
+		for(int i = 0; i < 3; i++) {
+			for(int j = 1; j < 4; j++) {
+				System.out.println(i + " " + j);
 			}
-			for (int i1 = 2; i1 < 501; i1++) {
-				if (i1 % 2 == 0) {
-					System.out.println(i1 + " is even!");
-				}else {
-					System.out.println(i1 + " is odd!");
-				}
-				int a = 7;
-			for (int g = 0; g < 111;g++) {
-				System.out.println(a);
-				a += 7;
-			}
+			for (int n = 0; n < 1; n++) {
 				
+			}
 			
-		
 		}
+		
 	}
-}
 }
